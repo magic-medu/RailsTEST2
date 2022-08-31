cb4b0de782b4a329477ec74b7230a9c7dd5c4631
magic-medu <sigure.natsu3@gmail.com>
2022-08-31 18:45:55 +0900

    mypageリンク飛べない

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resource :pages
+  resource :mypage
+  resource :users
+  devise_for :users
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
